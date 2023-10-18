0992a911961acbfe45d33d37a6a5f34e83d792d9
Tanman515 <104677232+Tanman515@users.noreply.github.com>
2023-10-18 21:11:16 +1000

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,8 +5,10 @@ def read_txt(path):
     with open(path, 'r') as f:
         return f.read()
 
+schema = 'tests/fixtures/'
+
 
 def test_gendiff_plane():
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == read_txt('tests/fixtures/expected1.txt')  #noqa
-    assert generate_diff('tests/fixtures/file2.json', 'tests/fixtures/file3.json') == read_txt('tests/fixtures/expected2.txt')  #noqa
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file3.json') == read_txt('tests/fixtures/expected3.txt')  #noqa
+    assert generate_diff(f'{schema}file1.json', f'{schema}file2.json') == read_txt(f'{schema}expected1.txt')  #noqa
+    assert generate_diff(f'{schema}file2.json', f'{schema}file3.json') == read_txt(f'{schema}expected2.txt')  #noqa
+    assert generate_diff(f'{schema}file1.json', f'{schema}file3.json') == read_txt(f'{schema}expected3.txt')  #noqa
