1f61ed4b1503824b2b01cf7f7165edb143806656
Oleg <antoshinov@gmail.com>
2023-11-05 21:04:12 +1000

    'add_new_tests'

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,17 +11,17 @@ schema = 'tests/fixtures/'
 
 def test_gendiff_json():
     assert generate_diff(f'{schema}file1.json', f'{schema}file2.json') == read_txt(f'{schema}expected1.txt')
-    assert generate_diff(f'{schema}file2.json', f'{schema}file3.json') == read_txt(f'{schema}expected2.txt')
-    assert generate_diff(f'{schema}file1.json', f'{schema}file3.json') == read_txt(f'{schema}expected3.txt')
 
 
 def test_gendiff_yaml():
     assert generate_diff(f'{schema}file1.yaml', f'{schema}file2.yaml') == read_txt(f'{schema}expected1.txt')
-    assert generate_diff(f'{schema}file2.yaml', f'{schema}file3.yaml') == read_txt(f'{schema}expected2.txt')
-    assert generate_diff(f'{schema}file1.yaml', f'{schema}file3.yaml') == read_txt(f'{schema}expected3.txt')
 
 
 def test_gendiff_with_inner_json():
     assert generate_diff(f'{schema}file1_inner.json', f'{schema}file2_inner.json') == read_txt(f'{schema}expected4.txt')
+
+
+def test_gendiff_with_inner_yaml():
+    assert generate_diff(f'{schema}file1_inner.yaml', f'{schema}file2_inner.yaml') == read_txt(f'{schema}expected4.txt')
 
 
